d8a62d434cc924433d306847c0c037031c02ba1d
sumit <sumitthapa90@gmail.com>
2021-12-09 18:01:11 +0530

    complete

--- block-BNaaee/google/modules/passport.js
+++ block-BNaaee/google/modules/passport.js
@@ -3,7 +3,7 @@ var User = require("../models/User");
 
 var GitHubStrategy = require("passport-github").Strategy;
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+var LocalStrategy = require("passport-local").Strategy;
 
 //github
 passport.use(
@@ -36,16 +36,56 @@ passport.use(
     }
   )
 );
-
-
 //google
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      var profileData = {
+        name: profile.displayName,
+        username: profile._json.name,
+        email: profile._json.email,
+        profilePik: profile._json.picture,
+      };
 
+      User.findOne({ email: profile._json.email }, (err, user) => {
+        if (err) return done(err);
+        if (!user) {
+          User.create(profileData, (err, addUser) => {
+            if (err) return done(err);
+            return done(null, addUser);
+          });
+        }
+        done(null, user);
+      });
+    }
+  )
+);
+
+//local
+
+passport.use(
+  new LocalStrategy(function (email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "incorrect username" });
+      }
+      if (!user.verifyPassword(password)) {
+        return done(null, false, { message: "incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
